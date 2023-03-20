6f964a2752f85c469b8c84eb344ff2b359270a10
Said Dautović <83250294+dautovicsaid@users.noreply.github.com>
2023-03-20 16:38:53 +0100

    Infinite scroll view url fix Users soft delete

---
+++ resources/js/Components/SuccessButton.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+    type: {
+        type: String,
+        default: 'submit',
+    },
+});
+</script>
+
+<template>
+    <button
+        :type="type"
+        class="inline-flex items-center rounded-md border border-transparent bg-green-500 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-green-400 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 active:bg-green-600 dark:focus:ring-offset-gray-800"
+    >
+        <slot />
+    </button>
+</template>
